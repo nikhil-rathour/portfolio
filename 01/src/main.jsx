609a46b20c5bcb44ca2d0baee1e303a1ad46a6d4
nikhil-rathour <rathournikhil042@gmail.com>
2025-05-11 11:44:20 +0530

    feat: add main entry point with React rendering and routing setup

---
+++ 01/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  
+      <App />
+    
+  </StrictMode>
+)
