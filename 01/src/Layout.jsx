ba30109b6504809c84c0274f8b1b8b3d6688daf8
nikhil-rathour <rathournikhil042@gmail.com>
2025-05-13 19:36:10 +0530

    feat: restructure Layout component to include stars background and improve layout organization

--- 01/src/Layout.jsx
+++ 01/src/Layout.jsx
@@ -1,17 +1,23 @@
-
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import StarsCanvas from './components/StarsCanvas' // Adjust the import path as needed
 
 function Layout() {
   return (
     <>
-    <Header/> 
-    <Outlet/>
-    <Footer/>
-    </>
-    
-     )
+   <div className="stars-background">
+  <StarsCanvas />
+</div>
+
+<div className="content-wrapper">
+  <Header />
+  <main className="content-section">
+    <Outlet />
+  </main>
+  <Footer />
+</div></>
+  )
 }
 
 export default Layout
