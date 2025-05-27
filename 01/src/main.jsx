07441da770d3f8382ba5c1cb06c463e6b6c8eba5
nikhil-rathour <rathournikhil042@gmail.com>
2025-05-27 11:21:10 +0530

    feat: add @vercel/speed-insights dependency and integrate SpeedInsights component in main.jsx

--- 01/src/main.jsx
+++ 01/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   
       <App />
+      <SpeedInsights/>
     
   </StrictMode>
 )
